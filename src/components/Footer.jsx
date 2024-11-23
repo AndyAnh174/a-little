2a8b180544ea662b258fcd98c1f7859e852d094b
unknown <hovietanh147@gmail.com>
2024-11-23 12:11:02 +0700

    04/12

---
+++ src/components/Footer.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="footer flex items-center justify-center p-10 bg-white text-gray-500">
+      <aside>
+        <p className="text-sm">Made with love and coffee by AndyAnh 🐱‍👓</p>
+        <span className="italic font-bold text-center text-sm w-full">
+          Note: VA xin lỗi Nhung vì hông nhắn xin hình Nhung 🎈
+        </span>
+      </aside>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ src/components/ImageSlide/stocks/index.jsx
@@ -0,0 +1,14 @@
+import IMG_1 from "./image1.jpg";
+import IMG_2 from "./image2.jpeg";
+import IMG_3 from "./image3.png";
+import IMG_4 from "./image4.png";
+
+export default [
+  { src: IMG_1, alt: "img1" },
+  { src: IMG_3, alt: "img3" },
+  { src: IMG_4, alt: "img4" },
+  {
+    src: IMG_2,
+    alt: "img2",
+  },
+];
